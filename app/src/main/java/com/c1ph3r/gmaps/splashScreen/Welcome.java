2a414c30abfb025f05fb1d1c5702babf5c3e149d
Jeeva Prakash <jeevarag1016@gmail.com>
2022-12-21 10:08:26 +0530

    commit

--- app/src/main/java/com/c1ph3r/gmaps/splashScreen/Welcome.java
+++ app/src/main/java/com/c1ph3r/gmaps/splashScreen/Welcome.java
@@ -1,15 +1,14 @@
 package com.c1ph3r.gmaps.splashScreen;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.PackageManagerCompat;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
@@ -23,7 +22,6 @@ import com.c1ph3r.gmaps.MainActivity;
 import com.c1ph3r.gmaps.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import java.security.Permission;
 
 public class Welcome extends AppCompatActivity {
 
@@ -55,40 +53,32 @@ public class Welcome extends AppCompatActivity {
             new MaterialAlertDialogBuilder(this).setTitle("Location Permission Required!")
                     .setMessage("We need your location to Show your location on map so, " +
                             "Please allow your location permission in the settings app to continue.")
-                    .setPositiveButton("Allow Permission", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent = new Intent();
-                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                            Uri uri = Uri.fromParts("package",getPackageName(), null);
-                            intent.setData(uri);
-                            startActivity(intent);
-                        }
+                    .setPositiveButton("Allow Permission", (dialogInterface, i) -> {
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package",getPackageName(), null);
+                        intent.setData(uri);
+                        getPermissionResult.launch(intent);
                     }).show();
         }else{
             askUserForPermission();
         }
     }
+
+    public ActivityResultLauncher<Intent> getPermissionResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> askUserForPermission());
+
     private void getUserLocationPermission() {
         if(!(isNetworkConnected())){
             new MaterialAlertDialogBuilder(this).setTitle("Internet connection required!")
                     .setMessage("Please turn on your internet connection and try again.")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            startActivity(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS));
-                        }
-                    }).show();
+                    .setPositiveButton("Ok", (dialogInterface, i) -> startActivity(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS))).show();
         }
         else if(!(checkGPSStatus())){
             new MaterialAlertDialogBuilder(this).setTitle("Internet connection required!")
                     .setMessage("Please turn on your internet connection and try again.")
-                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                            startActivity(intent);
-                        }
+                    .setPositiveButton("Ok", (dialogInterface, i) -> {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
                     }).show();
         }
         else {
@@ -103,7 +93,7 @@ public class Welcome extends AppCompatActivity {
     }
 
     private boolean checkGPSStatus() {
-        LocationManager locationManager = null;
+        LocationManager locationManager;
         boolean gps_enabled = false;
         boolean network_enabled = false;
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
